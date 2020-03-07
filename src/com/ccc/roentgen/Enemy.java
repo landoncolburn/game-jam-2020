0d4c8bae84ef25694b8ea73b7a55d1fdb1e2174d
Landon Colburn <colburnlandon@gmail.com>
2020-03-07 02:35:49 -0600

    Added Basic Pathfinding

--- src/com/ccc/roentgen/Enemy.java
+++ src/com/ccc/roentgen/Enemy.java
@@ -5,27 +5,36 @@ import java.awt.image.BufferedImage;
 
 public class Enemy extends GameObject {
 	
-	private int velX = 2;
-	private int velY = 2;
-	
-	private int count = 0;
+	private double velX = 0;
+	private double velY = 0;
+	private GameObject target;
 	
 	private BufferedImage sprite;
 
 	public Enemy(int x, int y) {
 		super(x, y, 64, 96, true, ID.ENEMY);
 		sprite = BufferedImageLoader.loadImage("enemy.png");
+		target = Game.gameInstance.handler.getByID(ID.PLAYER).get(0);
 	}
 
 	@Override
 	public void tick() {
 		x+=velX;
 		y+=velY;
-		count++;
-		if(count>200) {
-			velX *= -1;
-			velY *= -1;
-			count = 0;
+		if(Math.abs(x-target.getX())<5) {
+			velX = 0;
+		} else if(x<target.getX()) {
+			velX = 1;
+		} else if(x>target.getX()) {
+			velX = -1;
+		}
+		
+		if(Math.abs(y-target.getY())<5) {
+			velY = 0;
+		} else if(y<target.getY()) {
+			velY = 1;
+		} else if(y>target.getY()) {
+			velY = -1;
 		}
 	}
 
